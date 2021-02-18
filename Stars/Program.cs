c0a2c4103a61fc76ef2fb17ca0546d0366d4e120
hallamjason4 <70266362+hallamjason4@users.noreply.github.com>
2021-02-18 16:22:28 +0000

    Add files via upload
    A pyramid of stars

---
+++ Stars/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of rows: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n+1; i++)
+            {
+                for (int j = 0; j < n-i; j++)
+                    Console.Write(" ");
+                for (int k = 0; k < 2*i-1; k++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
